c0959d589188e88cb77d6bf73cb934eb6a0c181b
travislin5 <67934666+travislin5@users.noreply.github.com>
2024-05-28 01:02:16 +0800

    test

--- main.py
+++ main.py
@@ -105,3 +105,9 @@ def get_company_data(request: CompanyRequest):
 
     finally:
         driver.quit()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app="start:app", host="0.0.0.0", port="8080", reload=True)
